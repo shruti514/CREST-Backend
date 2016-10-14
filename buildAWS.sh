13170738bc2ba2019b7670fc967b93216ee69c06
Shrutee Gangras <sgangras@stubhub.com>
2016-10-14 16:56:33 -0700

    updating pom file

--- buildAWS.sh
+++ buildAWS.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mvn clean package
 echo "[INFO] ---------------------  Copying artifact to Tomcat ----------------------"
-sudo cp target/crest-backend-1.0-SNAPSHOT.jar /usr/share/tomcat7/webapps/
+sudo cp target/crest-backend-1.0-SNAPSHOT.war /usr/share/tomcat7/webapps/
 echo "[INFO] ---------------- Successfully copied artifact to Tomcat ----------------"
