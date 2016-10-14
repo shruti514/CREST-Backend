e72742d83223c5a200340c7dddb979ab87f6c5b4
Shrutee Gangras <sgangras@stubhub.com>
2016-10-14 15:29:40 -0700

    some changes

--- buildAWS.sh
+++ buildAWS.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 mvn clean package
+echo "------------------ Copying artifact to tomcat --------------------"
 sudo cp target/crest-backend-1.0-SNAPSHOT.jar /usr/share/tomcat7/webapps/
+echo "------------- Successfully copied artifact to tomcat -------------"
